ccb72c61052980d0abe83cbfa70d4887f685c53a
chic-page-6502 <zacharytq@gmail.com>
2019-08-09 01:02:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,4 +6,32 @@ class Book
   def title
     @title
   end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
